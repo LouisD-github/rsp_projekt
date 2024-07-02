5bc50606f70fad419503223d87c8c635def59ef2
FlyingNikki <niklasnagel2@outlook.de>
2024-07-02 10:13:51 +0200

    klassen added

---
+++ Programmierung/Buchhaltung/src/Geschäftsführer.java
@@ -0,0 +1,4 @@
+
+public class Geschäftsführer extends Mitarbeiter{
+
+}
--- Programmierung/Buchhaltung/src/Main_Buchhaltung.java
+++ Programmierung/Buchhaltung/src/Main_Buchhaltung.java
@@ -2,7 +2,7 @@
 public class Main_Buchhaltung {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
